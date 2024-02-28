b19bfebb3ef74218a4f3f00c012982ffb0fd0b3c
patrick okafor <patrickpeters911@gmail.com>
2024-02-28 09:06:52 -0800

    SSH client configuration must be configured to refuse to authenticate using a password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,27 +1,12 @@
-# using Puppet to make changes to our configuration file so that you can 
-# connect to a server without typing a password.
-
-file { ' /etc/ssh/ssh_config ':
-	ensure => file,
-	owner => 'root',
-	group => 'root',
-	mode => '0644',
+# Client configuration file (w/ Puppet)
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
-# Disable Password authentication
-
-file_line { 'Turn of passwd auth':
-	path => '/etc/ssh/ssh_config',
-	line => 'PasswordAuthentication no',
-	match => '^#?PasswordAuthentication.*',
-	ensure => present,
-}
-
-# Declare Identity file
-
-file_line = { 'Declare identity file':
-	path => '/etc/ssh/shh_config',
-	line => 'IdentityFile ~/.ssh/school',
-	match => '^#IdentutyFile.*',
-	ensure => present,
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
