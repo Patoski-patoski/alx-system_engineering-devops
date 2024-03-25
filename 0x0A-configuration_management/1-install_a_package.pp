fc4325f17a3d19f64fcebe375c728da77e09b431
root <Okafor Patrick C>
2024-03-25 14:35:23 -0700

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # Using Puppet, install flask from pip3
 
 package { 'flask':
- provider => 'pip3',
- ensure   => '2.1.1',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
 package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
-  ensure   => '2.1.0',
 }
