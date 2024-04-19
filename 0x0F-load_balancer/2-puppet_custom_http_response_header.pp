06bbdc4027709cfdd82ca7043cc56e40fb9186e5
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-18 23:44:26 -0700

    o automate the task of creating a custom HTTP header response, but with Puppet.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,54 +1,30 @@
-# Define class to install and configure nginx
-class web_server {
-    package { 'nginx':
-        ensure  => installed,
-        require => Apt::Update,
-    }
+# Define a class for configuring Nginx with custom HTTP header
+class nginx_custom_header {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-    file { '/var/www/html/index.html':
-        ensure  => present,
-        content => 'Hello World!',
-        owner   => 'www-data',
-        group   => 'www-data',
-        mode    => '0644',
-        require => Package['nginx'],
-    }
+  file { '/etc/nginx/conf.d/custom_header.conf':
+    ensure  => present,
+    content => "server {
+                  listen 80 default_server;
+                  server_name _;
 
-    file { '/etc/nginx/sites-available/default':
-        ensure  => present,
-        content => template('web_server/nginx_default.erb'),
-        require => Package['nginx'],
-        notify  => Service['nginx'],
-    }
+                  location / {
+                      add_header X-Served-By $hostname;
+                      # Other configuration directives...
+                  }
+                }",
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
 
-    file { '/etc/nginx/sites-enabled/default':
-        ensure  => link,
-        target  => '/etc/nginx/sites-available/default',
-        require => File['/etc/nginx/sites-available/default'],
-        notify  => Service['nginx'],
-    }
-
-    service { 'nginx':
-        ensure    => running,
-        enable    => true,
-        hasrestart => true,
-        require   => [Package['nginx'], File['/etc/nginx/sites-available/default']],
-    }
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+  }
 }
 
-# Define template for nginx default site configuration
-# This template will be used to generate the nginx configuration file
-# /etc/nginx/sites-available/default
-# You need to create a file named nginx_default.erb in your Puppet module's templates directory
-# with the following content:
-#
-# server {
-#     listen 80;
-#     listen [::]:80;
-#     root /var/www/html;
-#     index index.html;
-#     location / {
-#         try_files $uri $uri/ =404;
-#     }
-#     add_header X-Served-By 305199-web-01;
-# }
+# Apply the class to configure Nginx with custom header
+include nginx_custom_header
