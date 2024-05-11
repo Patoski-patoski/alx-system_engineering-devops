2c0dfeccd3bbef90cc69e7457d1627e29f93aee1
patrick okafor <patrickpeters911@gmail.com>
2024-05-10 20:29:22 -0700

    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,75 +1,38 @@
 #!/usr/bin/python3
-"""0-gather_data_from_an_API"""
-
 import requests
 from sys import argv
 
+url_users = f"https://jsonplaceholder.typicode.com/users"
+url_todo = f"https://jsonplaceholder.typicode.com/todos"
 
-def get_name(users, employee_id) -> None:
-    """
-    Gets the names of the employee
+employee_id = int(argv[1])
+TASKS_DONE = 0
+TOTAL_NUM_TASKS = 0
+TASK_TITLE = ""
 
-    Arg:
-        users (dict): The first argument
-        employee_id (int): The second argument
+users_resp = requests.get(url=url_users)
+todos_resp = requests.get(url=url_todo)
 
-    Return:
-        None
-    """
+users = users_resp.json()
+todos = todos_resp.json()
 
-    for user in users:
-        if user['id'] == employee_id:
-            return user['name']
+save_id = ""
+employee_name = ""
+for user in users:
+    if user['id'] == employee_id:
+        employee_name = user['name']
+        break
 
-
-def get_employee_tasks(todos, employee_id):
-    """
-    Gets the employee's tasks details
-
-    Arg:
-        todos (dict): The first argument
-        employee_id (int): The second argument
-
-    Return:
-        int: total_tasks
-        int: completed tasks
-        str: completed_task_titles
-    """
-    total_tasks = 0
-    completed_tasks = 0
-    completed_task_titles = []
-    for todo in todos:
-        if todo['userId'] == employee_id:
-            total_tasks += 1
-            if todo['completed'] is True:
-                completed_tasks += 1
-                completed_task_titles.append(todo['title'])
-
-    return (total_tasks, completed_tasks, completed_task_titles)
+for todo in todos:
+    if todo['userId'] == employee_id:
+        TOTAL_NUM_TASKS += 1
+        if todo['completed'] is True:
+            TASKS_DONE += 1
 
 
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        exit(1)
+print(f"{employee_name} is done with task ({TASKS_DONE}/{TOTAL_NUM_TASKS}):")
 
-    employee_id = int(argv[1])
-    url_users = "https://jsonplaceholder.typicode.com/users"
-    url_todos = "https://jsonplaceholder.typicode.com/todos"
-
-    users_resp = requests.get(url_users)
-    todos_resp = requests.get(url_todos)
-
-    users = users_resp.json()
-    todos = todos_resp.json()
-
-    employee_name = get_name(users, employee_id)
-    if employee_name is None:
-        print(f"Employee with ID {employee_id} not found.")
-        exit(1)
-
-    all_tasks, complete_tasks, titles = get_employee_tasks(todos, employee_id)
-
-    print(f"{employee_name} is done with task ({complete_tasks}/{all_tasks}):")
-    for title in titles:
-        print(f"\t{title}")
+for todo in todos:
+    if todo['completed'] is True and todo['userId'] is employee_id:
+        TASK_TITLE = todo['title']
+        print(f"\t{TASK_TITLE}")
