1083e4d5f8f785b9413e9a240439474d10dc0b2c
patrick okafor <patrickpeters911@gmail.com>
2024-05-12 08:48:07 -0700

    Python script to export data in the JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""  Python script to export data in the JSON format """
-
-if __name__ == "__main__":
+"""A Python script that, for a given employee ID,
+export infomation about his/her TODO list progress data in the JSON format.
+"""
+if __name__ == '__main__':
     import json
     import requests
-    from sys import argv
+    import sys
 
-    if len(argv) != 2:
-        print(f"Usage: python {argv[0]}  {argv[1]}")
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} <user_id>', file=sys.stderr)
         exit(1)
 
-    user_id = argv[1]
-    users = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos/"
+    user_id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    name = requests.get(url).json().get('username')
 
-    main_name = requests.get(users).json().get('username')
-    todos_resp = requests.get(todos).json()
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+    tasks = requests.get(url).json()
 
-    with open(f"{user_id}.json", 'w', encoding='utf-8') as json_file:
+    with open(f'{user_id}.json', 'w', encoding='utf-8') as jsonfile:
         json.dump({user_id: [{
-            'task': todo.get('title'),
-            'completed': todo.get('completed'),
-            'username': main_name
-            } for todo in todos_resp]}, json_file, indent=2)
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': name
+        } for task in tasks]}, jsonfile)
