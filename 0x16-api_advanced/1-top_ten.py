8a196d6d0e570fd321f74d46bff32d1615821788
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-05 09:37:46 -0700

    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 1.top_ten module"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """  a function that queries the Reddit API and prints the titles of the
+         first 10 hot posts listed for a given subreddit.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "1-top_ten"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code in [200, 302] and response.text.strip():
+        data = response.json()
+        for i, child in enumerate(data.get('data', {}).get('children', [])):
+            if i >= 10:
+                break
+            print(child.get('data', {}).get('title'))
+        return
+    return None
