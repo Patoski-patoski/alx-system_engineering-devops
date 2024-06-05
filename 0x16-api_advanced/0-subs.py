5e2b0a62056c786333acc68b476bb6071496955a
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-05 08:06:43 -0700

    returns a code that rthe number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,14 @@ def number_of_subscribers(subreddit):
     """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    try:
-        # Set a custom User-Agent to avoid rate limiting
-        headers = {'User-Agent': 'my_reddit_api_client'}
+    # Set a custom User-Agent to avoid rate limiting
+    headers = {'User-Agent': 'my_reddit_api_client'}
 
-        # Make a GET request to the subreddit's info endpoint
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    # Make a GET request to the subreddit's info endpoint
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data').get('subscribers')
-        else:
-            return 0
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+
+    return 0
