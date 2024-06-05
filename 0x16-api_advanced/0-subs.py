fbd19ef3b86bd20334836a55830a4665171b9e68
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-05 08:02:54 -0700

    returnsa code that rthe number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # a function that queries the Reddit API and returns the number of subscribers
 # (not active users, total subscribers) for a given subreddit. If an invalid
-# subreddit is given, the function should return 0.
+# subreddit is given, the function should return 0
 
 import requests
 
 
 def number_of_subscribers(subreddit):
 
-    link = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     try:
         # Set a custom User-Agent to avoid rate limiting
         headers = {'User-Agent': 'my_reddit_api_client'}
 
         # Make a GET request to the subreddit's info endpoint
-        response = requests.get(link, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            return data.get('data').get('subscribers')
         else:
             return 0
-    except (requests.exceptions.RequestException, KeyError):
-        return 0
