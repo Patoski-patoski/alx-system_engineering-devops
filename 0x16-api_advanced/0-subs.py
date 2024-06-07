946e24ee8d1d71b5a608aa38cdf5be7233ed9414
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-07 08:22:01 -0700

    added auth

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ 0-subs module"""
 import requests
+import requests.auth
 
+CLIENT_ID = "ZHsHCcoQxH9mxjKajUXjYA"
+SECRET_KEY = "7KL3Fy26ayJojzjighyIg6OjNI3BMQ"
+
+auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
 def number_of_subscribers(subreddit):
     """ a function that queries the Reddit API and returns the number of
@@ -10,12 +15,12 @@ def number_of_subscribers(subreddit):
         return 0
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     # Set a custom User-Agent to avoid rate limiting
-    headers = {'User-Agent': 'my_reddit_api_client'}
+    headers = {'User-Agent': '0-subs.py'}
 
     # Make a GET request to the subreddit's info endpoint
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False, auth=auth)
 
     if response.status_code == 200:
         data = response.json()
