a14bdd31dee7f7778064cb0b8bbad5e6687daa85
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-07 08:35:18 -0700

    added auth

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,12 @@
 """ 1.top_ten module"""
 
 import requests
+import requests.auth
 
+CLIENT_ID = "ZHsHCcoQxH9mxjKajUXjYA"
+SECRET_KEY = "7KL3Fy26ayJojzjighyIg6OjNI3BMQ"
+
+auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
 def top_ten(subreddit):
     """  a function that queries the Reddit API and prints the titles of the
@@ -11,7 +16,8 @@ def top_ten(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "1-top_ten"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(
+            url, headers=headers, allow_redirects=False, auth=auth)
 
     if response.status_code in [200, 302] and response.text.strip():
         data = response.json()
