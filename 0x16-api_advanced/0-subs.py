6c073b92ef113e010c78b65a40c949a571b1b4d7
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-07 08:55:40 -0700

    remove auth

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """ 0-subs module"""
 import requests
-import requests.auth
-
-CLIENT_ID = "ZHsHCcoQxH9mxjKajUXjYA"
-SECRET_KEY = "7KL3Fy26ayJojzjighyIg6OjNI3BMQ"
-
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
 
 def number_of_subscribers(subreddit):
@@ -18,15 +12,13 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     # Set a custom User-Agent to avoid rate limiting
-    headers = {'User-Agent': '0-subs.py'}
+    headers = {"User-Agent": "0-subs.py"}
 
     # Make a GET request to the subreddit's info endpoint
-    response = requests.get(
-            url, headers=headers, allow_redirects=False,
-            auth=auth)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
+    if response.status_code in [200, 302]:
         data = response.json()
-        return data.get('data').get('subscribers')
+        return data.get("data").get("subscribers")
 
     return 0
