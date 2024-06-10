34c7f9c80b3fc26df84079a3e4f03934ebb3225a
Okafor Patrick C <patrickpeters911@gmail.com>
2024-06-09 21:53:20 -0700

    initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,14 @@ def number_of_subscribers(subreddit):
         return 0
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     # Set a custom User-Agent to avoid rate limiting
     headers = {'User-Agent': 'my_reddit_api_client'}
 
     # Make a GET request to the subreddit's info endpoint
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
+    if response.status_code <= 200:
         data = response.json()
         return data.get('data').get('subscribers')
 
